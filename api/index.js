7ab6dbf732e973641d0ae78d40f84c5d83d1f864
AgusCue <cuelloagustin3@gmail.com>
2022-08-09 16:51:18 -0300

    deployar

--- api/index.js
+++ api/index.js
@@ -20,17 +20,8 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-// Syncing all the models at once.
-// conn.sync({ force: false }).then(() => {
-//   server.listen(3001, () => {
-//     console.log("%s listening at 3001"); // eslint-disable-line no-console
-//   });
-// });
-
-const { PORT } = process.env;
-
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`Listen on port ${PORT}`); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3001");
   });
 });
