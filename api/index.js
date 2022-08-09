128aec69ca8e0a4491045cbdf881a87afba73eba
AgusCue <cuelloagustin3@gmail.com>
2022-08-09 16:30:37 -0300

    deploy ultimo

--- api/index.js
+++ api/index.js
@@ -27,8 +27,8 @@ const { conn } = require("./src/db.js");
 //   });
 // });
 
+const { PORT } = process.env;
+
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
-  });
+  server.listen(PORT, () => console.log(`Listen on port ${PORT}`));
 });
