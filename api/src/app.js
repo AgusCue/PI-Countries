5c40bb36232f7628760eafde0d9e0db34ce79127
AgusCue <cuelloagustin3@gmail.com>
2022-08-10 20:35:29 -0300

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const bodyParser = require("body-parser");
-
 const morgan = require("morgan");
-
 const routes = require("./routes/index.js");
 
 require("./db.js");
 
 const server = express();
-// declaro una instancia de express
-server.use(cors());
+
 server.name = "API";
-//server.use(middelware)
+
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
